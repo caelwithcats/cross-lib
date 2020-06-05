23122b7f3144433c0f80c0c618ed493c960c342f
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-06-05 17:06:41 +1000

    Update packages.sh

--- cross_apis_r/packages.sh
+++ cross_apis_r/packages.sh
@@ -1,13 +1,24 @@
+actionsBuild = $1
 # The following packages are required for crossLib:
+if [ $actionsBuild == "true" ]
+then
+sudo apt-get -y install mingw-w64 g++ gcc build-essential libx11-dev
+elif
 sudo apt-get install mingw-w64 g++ gcc build-essential libx11-dev
+fi
 # For 32-bit support
 #sudo apt-get install g++-multilib libgdk-pixbuf2.0-dev:i386 libicu-dev:i386 libharfbuzz-dev:i386 libpango1.0-dev:i386
 
 # Optinal:
 # Good for testing windows .EXE files
+if [ $actionsBuild == "true" ]
+then
+echo "Done."
+elif
 read -p "Would you like to  install wine? Continue (y/n)?" choice
 case "$choice" in 
   y|Y ) echo Updating...; sudo apt update; echo "You should now be able to install WINE from the software centre (mintinstall if you are using linux mint)" 
   n|N ) echo "no";;
   * ) echo "invalid answer";;
 esac
+fi
