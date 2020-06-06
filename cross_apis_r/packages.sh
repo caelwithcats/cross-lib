5fbb90156e63d6c511300920822ac1acca56c0cc
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-06-06 12:34:14 +1000

    Update packages.sh

--- cross_apis_r/packages.sh
+++ cross_apis_r/packages.sh
@@ -1,10 +1,10 @@
-actionsBuild = $1
+actionsBuild=$1
 # The following packages are required for crossLib:
 if [ $actionsBuild == "true" ]
 then
-sudo apt-get -y install mingw-w64 g++ gcc build-essential libx11-dev
+  sudo apt-get -y install mingw-w64 g++ gcc build-essential libx11-dev
 elif
-sudo apt-get install mingw-w64 g++ gcc build-essential libx11-dev
+  sudo apt-get install mingw-w64 g++ gcc build-essential libx11-dev
 fi
 # For 32-bit support
 #sudo apt-get install g++-multilib libgdk-pixbuf2.0-dev:i386 libicu-dev:i386 libharfbuzz-dev:i386 libpango1.0-dev:i386
@@ -13,12 +13,12 @@ fi
 # Good for testing windows .EXE files
 if [ $actionsBuild == "true" ]
 then
-echo "Done."
+  echo "Done."
 elif
-read -p "Would you like to  install wine? Continue (y/n)?" choice
-case "$choice" in 
-  y|Y ) echo Updating...; sudo apt update; echo "You should now be able to install WINE from the software centre (mintinstall if you are using linux mint)" 
-  n|N ) echo "no";;
-  * ) echo "invalid answer";;
-esac
+  read -p "Would you like to  install wine? Continue (y/n)?" choice
+  case "$choice" in 
+    y|Y ) echo Updating...; sudo apt update; echo "You should now be able to install WINE from the software centre (mintinstall if you are using linux mint)" 
+    n|N ) echo "no";;
+    * ) echo "invalid answer";;
+  esac
 fi
