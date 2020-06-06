30d085b34496361433bce41995aeddc513906acf
Cael Rasmussen <53895123+caelwithcats@users.noreply.github.com>
2020-06-06 12:43:07 +1000

    Update packages.sh

--- cross_apis_r/packages.sh
+++ cross_apis_r/packages.sh
@@ -3,9 +3,10 @@ actionsBuild=$1
 if [ $actionsBuild == "true" ]
 then
   sudo apt-get -y install mingw-w64 g++ gcc build-essential libx11-dev
-elif
+else
   sudo apt-get install mingw-w64 g++ gcc build-essential libx11-dev
 fi
+
 # For 32-bit support
 #sudo apt-get install g++-multilib libgdk-pixbuf2.0-dev:i386 libicu-dev:i386 libharfbuzz-dev:i386 libpango1.0-dev:i386
 
@@ -14,7 +15,7 @@ fi
 if [ $actionsBuild == "true" ]
 then
   echo "Done."
-elif
+else
   read -p "Would you like to  install wine? Continue (y/n)?" choice
   case "$choice" in 
     y|Y ) echo Updating...; sudo apt update; echo "You should now be able to install WINE from the software centre (mintinstall if you are using linux mint)" 
